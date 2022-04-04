2880f98508d7662ee9fc0e53e72ed4e322e61b67
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2022-04-04 13:36:29 +0100

    DEP-3057 : Fix End chat dialog for screen readers (#76)
    * DEP-3057 : Fix End chat dialog for screen readers
    
    * Remove/Add new lines

---
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/views/EndChatPopup.spec.js
@@ -0,0 +1,48 @@
+import Popup from '../../../../../../../app/assets/javascripts/views/EndChatPopup'
+
+describe('EndChatPopup', () => {
+	let ecp;
+	let events = {};
+    let wrapper;
+
+	beforeEach(() => {
+        let container = document.createElement("div")
+		ecp = new Popup(container, "");
+
+        wrapper = ecp.wrapper;
+
+		events = {};
+
+		wrapper.addEventListener = jest.fn((event, callback) => {
+      		events[event] = callback;
+    	});
+      
+	});
+
+	test('Escape key closes end chat dialogie', () => {
+        jest.spyOn(ecp, 'onCancelEndChat').mockImplementation();
+
+        var event = new KeyboardEvent('keydown', {'key': 'Escape'});
+        wrapper.dispatchEvent(event);
+
+        expect(ecp.onCancelEndChat).toHaveBeenCalled();
+	});
+
+    test('Click on cancel end chat closes end chat dialogie', () => {
+        jest.spyOn(ecp, 'onCancelEndChat').mockImplementation();
+
+        var event = new KeyboardEvent('click', {});
+        wrapper.querySelector("#cancelEndChat").dispatchEvent(event);
+
+        expect(ecp.onCancelEndChat).toHaveBeenCalled();
+	});
+
+    test('Click on confirm end chat and end chat', () => {
+        jest.spyOn(ecp, 'onConfirmEndChat').mockImplementation();
+
+        var event = new KeyboardEvent('click', {});
+        wrapper.querySelector("#confirmEndChat").dispatchEvent(event);
+
+        expect(ecp.onConfirmEndChat).toHaveBeenCalled();
+	});
+});
