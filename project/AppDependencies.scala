4a73e23b51eaede5ecac5d7abc0050b15f0de03a
Pratik Rana <63359242+pratikrana06@users.noreply.github.com>
2022-05-25 12:33:28 +0100

    [DEP-3048][PR]:Library upgrades (#90)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
 
   val compile = Seq(
     "org.webjars.npm"         %  "hmrc-frontend"              % "1.35.2",
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.21.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.8.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.20.0-play-28",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.35.10"         % "test"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.21.0"            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"            % Test,
+    "org.jsoup"               %  "jsoup"                      % "1.15.1"            % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
   )
 
