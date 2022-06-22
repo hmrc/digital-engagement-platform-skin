ec59855688beaf5589ef409bef97e33c9ec6d43c
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2022-06-22 09:47:04 +0100

    DEP-2764 : Improve code coverage (#99)
    * DEP-2764: Added test to cover ClickToChatButton
    
    * Improve Chatcontainer coverage
    
    * remove logging
    
    * added new line
    
    Co-authored-by: Tony Myers <tonymyers@hotmail.com>

---
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/utils/ClickToChatButton.spec.js
@@ -0,0 +1,25 @@
+import ClickToChatButton from '../../../../../../../app/assets/javascripts/utils/ClickToChatButton'
+
+
+describe("ClickToChatButton", () => {
+    it("cteates a ClickToChatButton", () => {
+        
+        let div = document.createElement("div");
+        div.setAttribute("id", "HMRC_Fixed_1");
+        div.setAttribute("class", "hmrc-fixed-available");
+        document.body.appendChild(div);
+
+        let divId = document.createElement("div");
+        divId.setAttribute("class", "c2cButton");
+        divId.innerHTML = "ReadyText";
+       
+        const buttonClass = '<div class="hmrc-fixed-available">ReadyText</div>'
+
+        const clickToChatButton = new ClickToChatButton(divId, buttonClass);
+        
+        let clickToChatButtonSpy = jest.spyOn(divId, 'appendChild').mockImplementation();
+        clickToChatButton.replaceChild(div);
+
+        expect(clickToChatButtonSpy).toHaveBeenCalledTimes(1);
+    });
+});
