554ba573d5d94c3c08bb80d83df15ca49765865e
Tony Myers <tonymyers@hotmail.com>
2022-07-04 10:37:09 +0100

    DEP-3157: made chat ended text a H2

--- app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
+++ app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
@@ -1,9 +1,9 @@
 const html = `
 <div id="endPage">
     <div class="govuk-panel govuk-panel--confirmation" style="margin-right:0.8em;">
-        <h1 class="govuk-panel__title" id="heading_chat_ended" tabindex="-1">
+        <h2 class="govuk-panel__title" id="heading_chat_ended" tabindex="-1">
             Chat ended
-        </h1>
+        </h2>
     </div>
 
     <p id="endpage-thanks" class="govuk-body">Thank you for your feedback.</p>
