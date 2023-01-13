73c179c3982d45188f80a8dc952530441340fe61
Tony Myers <tonymyers@hotmail.com>
2023-01-13 11:36:46 +0000

    DEP-4102: Removed NINA code and tests, refactored some tests to cover still used functionality

--- app/assets/javascripts/NuanceMessageType.js
+++ app/assets/javascripts/NuanceMessageType.js
@@ -1,6 +1,5 @@
 // Message types as defined in the Nuance CI API documentation
 // These should not be changed.
-export const Chat_AutomationRequest = 'chat.automaton_request';
 export const Chat_Communication = 'chat.communication';
 export const Chat_CommunicationQueue = 'chat.communication.queue';
 export const Chat_NeedWait = 'chat.need_wait';
--- test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/NuanceMessageType.spec.js
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/NuanceMessageType.spec.js
@@ -3,7 +3,6 @@ import * as MessageType from '../../../../../../app/assets/javascripts/NuanceMes
 describe("MessageType", () => {
     it("has correct values matching Nuance docs", () => {
         expect(MessageType.Chat_Communication).toEqual('chat.communication')
-        expect(MessageType.Chat_AutomationRequest).toEqual('chat.automaton_request')
         expect(MessageType.Chat_CommunicationQueue).toEqual('chat.communication.queue')
         expect(MessageType.Chat_NeedWait).toEqual('chat.need_wait')
         expect(MessageType.Chat_Denied).toEqual('chat.denied')
