b34153af83ee63399a1cb99f439cb507b07af5fc
tomshaw1 <90316687+tomshaw1@users.noreply.github.com>
2023-03-08 13:57:27 +0000

    current progress

--- test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/utils/ClickToChatButtons.spec.js
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/utils/ClickToChatButtons.spec.js
@@ -25,7 +25,7 @@ function setup() {
 
 function c2cObj(displayState, launchable = false) {
     return {
-        c2cIdx: 'c2c-id',
+        c2cIdx: 'c2cId',
         displayState: displayState,
         launchable: launchable
     };
@@ -62,5 +62,29 @@ describe("ClickToChatButtons", () => {
         buttons.addButton(c2cObj(DisplayState.Busy), button);
 
         expect(button.replaceChild).toHaveBeenCalledWith('<div class="button-class busy">BusyText</div>');
+    });
+
+    it("updates all buttons to ChatActive state", () => {
+        const [, buttons, button] = setup();
+
+        buttons.addButton(c2cObj(DisplayState.OutOfHours), button);
+        buttons.addButton(c2cObj(DisplayState.Ready), button);
+        buttons.addButton(c2cObj(DisplayState.Busy), button);
+      
+        buttons.updateC2CButtonsToInProgress();
+      
+        expect(button.replaceChild).toHaveBeenNthCalledWith(1, '<div class="button-class chatactive">ChatActiveText</div>');
+        expect(button.replaceChild).toHaveBeenNthCalledWith(2, '<div class="button-class chatactive">ChatActiveText</div>');
+        expect(button.replaceChild).toHaveBeenNthCalledWith(3, '<div class="button-class chatactive">ChatActiveText</div>');
+      });
+
+    it("returns message for unknown state", () => {
+        const [, buttons, ] = setup();
+
+        const unknownState = "UnknownState";
+    
+        const result = buttons._getDisplayStateText(unknownState);
+    
+        expect(result).toContain("Unknown display state");
     });
 });
