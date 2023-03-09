3c72d3a8515301d5f3feac454a4ea69caf2bb828
machinesleet <34985534+machinesleet@users.noreply.github.com>
2023-03-09 15:07:23 +0000

    Dep 4277 - ClickToChatButtons test (#143)
    * DEP-4277  ClickToChatButtons test

--- test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/utils/ClickToChatButtons.spec.js
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/utils/ClickToChatButtons.spec.js
@@ -8,6 +8,8 @@ const displayStateMessages = {
     [DisplayState.ChatActive]: "ChatActiveText"
 };
 
+const testButton = document.createElement("div");
+
 function setup() {
     const onClicked = jest.fn();
     return [
@@ -18,14 +20,14 @@ function setup() {
         // button
         {
             buttonClass: 'button-class',
-            replaceChild: jest.fn()
+            replaceChild: jest.fn(_ => { return testButton; })
         }
     ];
 }
 
 function c2cObj(displayState, launchable = false) {
     return {
-        c2cIdx: 'c2c-id',
+        c2cIdx: 'c2cId',
         displayState: displayState,
         launchable: launchable
     };
@@ -62,5 +64,39 @@ describe("ClickToChatButtons", () => {
         buttons.addButton(c2cObj(DisplayState.Busy), button);
 
         expect(button.replaceChild).toHaveBeenCalledWith('<div class="button-class busy">BusyText</div>');
+    });
+
+    it("updates button to ChatActive state", () => {
+        const [, buttons, button] = setup();
+
+        buttons.addButton(c2cObj(DisplayState.OutOfHours), button);
+      
+        buttons.updateC2CButtonsToInProgress();
+      
+
+        expect(button.replaceChild).toHaveBeenNthCalledWith(1, '<div class="button-class outofhours">OutOfHoursText</div>');
+        expect(button.replaceChild).toHaveBeenNthCalledWith(2, '<div class="button-class chatactive">ChatActiveText</div>');
+    });
+
+    it("returns message for unknown state", () => {
+        const [, buttons, ] = setup();
+
+        const unknownState = "UnknownState";
+    
+        const result = buttons._getDisplayStateText(unknownState);
+    
+        expect(result).toContain("Unknown display state");
+    });
+
+    it("sets an onclick function given the click to chat object is launchable", () => {
+        const [onClicked, buttons, button] = setup();
+        const launchable = true;
+
+        buttons.addButton(c2cObj(DisplayState.ChatActive, launchable), button);
+
+        testButton.click();
+
+        expect(onClicked).toBeCalledTimes(1);
+        expect(onClicked).toBeCalledWith("c2cId");
     });
 });
