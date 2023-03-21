23f119f6bf883da6b5a86facb696d0310b04ac9b
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2023-03-21 15:43:07 +0000

    Dep 4280 (#145)

--- test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/views/EndChatPopup.spec.js
+++ test/uk/gov/hmrc/digitalengagementplatformskin/javascripts/views/EndChatPopup.spec.js
@@ -1,48 +1,77 @@
 import Popup from '../../../../../../../app/assets/javascripts/views/EndChatPopup'
 
 describe('EndChatPopup', () => {
-	let ecp;
+	let endChatPopup;
 	let events = {};
     let wrapper;
+    let eventHandler = {
+        onCancelEndChat: jest.fn(),
+        onConfirmEndChat: jest.fn()
+    };
 
 	beforeEach(() => {
-        let container = document.createElement("div")
-		ecp = new Popup(container, "");
+        jest.clearAllMocks();
 
-        wrapper = ecp.wrapper;
+        let container = document.createElement("div");
+
+        endChatPopup = new Popup(container, eventHandler);
+
+        wrapper = endChatPopup.wrapper;
 
 		events = {};
 
 		wrapper.addEventListener = jest.fn((event, callback) => {
       		events[event] = callback;
     	});
-      
 	});
 
 	test('Escape key closes end chat dialogue', () => {
-        jest.spyOn(ecp, 'onCancelEndChat').mockImplementation();
+        jest.spyOn(endChatPopup, 'onCancelEndChat').mockImplementation();
 
         var event = new KeyboardEvent('keydown', {'key': 'Escape'});
         wrapper.dispatchEvent(event);
 
-        expect(ecp.onCancelEndChat).toHaveBeenCalled();
+        expect(endChatPopup.onCancelEndChat).toHaveBeenCalled();
 	});
 
     test('Click on cancel end chat closes end chat dialogue', () => {
-        jest.spyOn(ecp, 'onCancelEndChat').mockImplementation();
+        jest.spyOn(endChatPopup, 'onCancelEndChat').mockImplementation();
 
         var event = new KeyboardEvent('click', {});
         wrapper.querySelector("#cancelEndChat").dispatchEvent(event);
 
-        expect(ecp.onCancelEndChat).toHaveBeenCalled();
+        expect(endChatPopup.onCancelEndChat).toHaveBeenCalled();
 	});
 
     test('Click on confirm end chat and end chat', () => {
-        jest.spyOn(ecp, 'onConfirmEndChat').mockImplementation();
+        jest.spyOn(endChatPopup, 'onConfirmEndChat').mockImplementation();
 
         var event = new KeyboardEvent('click', {});
         wrapper.querySelector("#confirmEndChat").dispatchEvent(event);
 
-        expect(ecp.onConfirmEndChat).toHaveBeenCalled();
+        expect(endChatPopup.onConfirmEndChat).toHaveBeenCalled();
 	});
+
+	test('Click on confirm end chat calls the expected method on the eventHandler', () => {
+        const evt = { preventDefault: jest.fn() };
+        endChatPopup.onConfirmEndChat(evt);
+
+        expect(eventHandler.onConfirmEndChat).toHaveBeenCalledTimes(1);
+    });
+
+    test('Click on cancel end chat calls the expected method on the eventHandler', () => {
+        const evt = { preventDefault: jest.fn() };
+        endChatPopup.onCancelEndChat(evt);
+
+        expect(eventHandler.onCancelEndChat).toHaveBeenCalledTimes(1);
+    });
+
+    test('Calls _setDisplay with the expected state, and sets the wrapper style display property', () => {
+        let endChatPopupSpy = jest.spyOn(endChatPopup, '_setDisplay');
+        
+        endChatPopup.show();
+
+        expect(endChatPopupSpy).toBeCalledWith("block");
+        expect(wrapper.style.display).toBe("block");
+    });
 });
