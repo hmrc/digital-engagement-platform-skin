ffa517efab88ef5894e135e011239f3055a9cd37
dantate92 <daniel.tate@digital.hmrc.gov.uk>
2023-06-14 15:32:01 +0100

    first draft of pcs page

--- app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
+++ app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
@@ -5,19 +5,35 @@ const html = `
             Chat ended
         </h2>
     </div>
+    <div id ='futherLinks'>
+        <p>You can:</p>
+        <ul>
+            <li><a href='#' id='printButton'>print or save your chat</a></li>    
+            <li><a href="http://www.gov.uk">return to GOV.UK</a></li>
+            <li>close this window</li>
+        </ul>
+    </div>
 
     <p id="endpage-thanks" class="govuk-body">Thank you for your feedback.</p>
 
 </div>
 `
 
+const nullEventHandler = {
+    onPrint: function () {}
+};
+
 export default class PostPCSPage {
+    
     constructor(showThanks) {
         this.showThanks = showThanks;
+        this.eventHandler = nullEventHandler;
+        this._registerEventListeners();
+        this.printPreview();
     }
+    
     attachTo(container) {
         this.container = container;
-
         this.wrapper = document.createElement("div")
         this.wrapper.id = "postPCSPageWrapper";
         this.wrapper.insertAdjacentHTML("beforeend", html);
@@ -25,6 +41,21 @@ export default class PostPCSPage {
             this.wrapper.querySelector('#endpage-thanks').style.display = 'none';
         }
         container.appendChild(this.wrapper);
+    }
+
+    _registerEventListener(selector, handler) {
+        const element = this.wrapper.querySelector(selector);
+        if (element) {
+            element.addEventListener("click", handler);
+        }
+    }
+
+    _registerEventListeners() {
+        this._registerEventListener("#printButton", (e) => {
+            this.eventHandler.onPrint(e);
+            console.log('************************************hi')
+            e.preventDefault();
+        });
     }
 
     detach() {
