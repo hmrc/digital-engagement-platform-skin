4c0df0a9ec32ca2c5ecf0a72f8a90bf7fa55de99
dantate92 <daniel.tate@digital.hmrc.gov.uk>
2023-06-23 16:21:59 +0100

    print preview working

--- app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
+++ app/assets/javascripts/views/postChatSurvey/PostPCSPage.js
@@ -1,3 +1,5 @@
+import PrintUtils from "../../utils/PrintUtils"
+
 const html = `
 <div id="endPage">
     <div class="govuk-panel govuk-panel--confirmation" style="margin-right:0.8em;">
@@ -8,7 +10,8 @@ const html = `
     <div id ='futherLinks'>
         <p>You can:</p>
         <ul>
-            <li><a href='#' id='printButton'>print or save your chat</a></li>    
+        
+        <li><a href='#' id='printButton'>print or save your chat</a></li>
             <li><a href="http://www.gov.uk">return to GOV.UK</a></li>
             <li>close this window</li>
         </ul>
@@ -27,35 +30,64 @@ export default class PostPCSPage {
     
     constructor(showThanks) {
         this.showThanks = showThanks;
+        this.container = document.createElement("div");
+        this.container.id = "ciapiSkin";
         this.eventHandler = nullEventHandler;
-        this._registerEventListeners();
-        this.printPreview();
+        this.content = this.container.querySelector("#endPage");
     }
-    
+
     attachTo(container) {
         this.container = container;
         this.wrapper = document.createElement("div")
         this.wrapper.id = "postPCSPageWrapper";
         this.wrapper.insertAdjacentHTML("beforeend", html);
+
         if (!this.showThanks) {
             this.wrapper.querySelector('#endpage-thanks').style.display = 'none';
         }
+        
         container.appendChild(this.wrapper);
-    }
 
-    _registerEventListener(selector, handler) {
-        const element = this.wrapper.querySelector(selector);
+        const element = this.wrapper.querySelector('#printButton');
         if (element) {
-            element.addEventListener("click", handler);
-        }
-    }
+            element.addEventListener("click", (e) => {
 
-    _registerEventListeners() {
-        this._registerEventListener("#printButton", (e) => {
-            this.eventHandler.onPrint(e);
-            console.log('************************************hi')
-            e.preventDefault();
-        });
+                const elementList = [
+                    "app-related-items",
+                    "govuk-back-link",
+                    "govuk-phase-banner",
+                    "hmrc-report-technical-issue",
+                    "govuk-footer",
+                    "govuk-heading-xl",
+                    "hmrc-user-research-banner",
+                    "cbanner-govuk-cookie-banner",
+                    "postChatSurveyWrapper",
+                    "govuk-grid-column-two-thirds"
+                ];
+          
+                PrintUtils.removeElementsForPrint(elementList);
+
+                const endPageWrapper = this.container.querySelector('#endPage')
+                const skinChatTranscript = this.container.querySelector("#ciapiSkinChatTranscript");
+
+                endPageWrapper.style.display = 'none'
+                skinChatTranscript.style.display = ''
+
+                window.print();
+                    this.eventHandler.onPrint(e);      
+                    e.preventDefault();
+                })
+
+                    window.addEventListener("afterprint", (e) => {});
+                    onafterprint = (e) => {
+                        e.preventDefault();
+                        const endPageWrapper = this.container.querySelector('#endPage')
+                        const skinChatTranscript = this.container.querySelector("#ciapiSkinChatTranscript");
+
+                        endPageWrapper.style.display = ''
+                        skinChatTranscript.style.display = 'none'
+                    };
+        }
     }
 
     detach() {
