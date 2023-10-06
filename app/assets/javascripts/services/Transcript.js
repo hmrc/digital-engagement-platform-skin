bb91769c162e4e964ffad9eea108ff48373a00dd
James Forster <14073802+jameshforster@users.noreply.github.com>
2023-10-06 16:38:22 +0100

    DEP-5740 Updated fix to not require html changes.

--- app/assets/javascripts/services/Transcript.js
+++ app/assets/javascripts/services/Transcript.js
@@ -286,10 +286,6 @@ export default class Transcript {
             let nodeContainer = document.createElement("div");
             nodeContainer.classList.add(initialNode.id);
 
-            let widgetContainer = document.createElement("div")
-
-            divContainer.append(widgetContainer)
-
             for(let i = 0; i < Object.keys(initialNode.controls).length; i++) {
                 let control = initialNode.controls[i];
 
@@ -301,12 +297,13 @@ export default class Transcript {
                     break;
                 }
 
-                widgetContainer.append(renderedControl);
+                divContainer.append(renderedControl);
             }
 
             if (!!quickReplyData.transitions) {
-                widgetContainer.transitions = quickReplyData.transitions;
-                widgetContainer.addEventListener('click', this.handleRichMediaClickEvent);
+                let quickReplyWidget = divContainer.getElementsByClassName("quick-reply-widget")[0]
+                quickReplyWidget.transitions = quickReplyData.transitions;
+                quickReplyWidget.addEventListener('click', this.handleRichMediaClickEvent);
             }
 
             const isQuickReply = true;
