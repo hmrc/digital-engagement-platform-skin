7fda2071043c3baeca0b064c6cdf5ff4309915ae
James Forster <14073802+jameshforster@users.noreply.github.com>
2023-10-18 12:44:24 +0100

    Fix class check for id check.

--- app/assets/javascripts/chat-ui.js
+++ app/assets/javascripts/chat-ui.js
@@ -39,9 +39,9 @@ export function hookWindow(w, commonChatController, reactiveChatController, proa
         }
     );
 
-    w.nuanceReactive_HMRC_CIAPI_Anchored_1 = safeHandler(
+        w.nuanceReactive_HMRC_CIAPI_Anchored_1 = safeHandler(
         function nuanceReactive_HMRC_CIAPI_Anchored_1(c2cObj) {
-            if (document.getElementsByClassName("tc-nuance-chat-container").length > 0) {
+            if (document.getElementById("tc-nuance-chat-container").length > 0) {
                 reactiveChatController.addC2CButton(c2cObj, "tc-nuance-chat-container", "anchored");
             } else {
                 reactiveChatController.addC2CButton(c2cObj, "HMRC_CIAPI_Anchored_1", "anchored");
