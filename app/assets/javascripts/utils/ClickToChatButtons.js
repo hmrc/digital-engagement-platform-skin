7e083bd3fc9a6dbc9eac5b34b867f1d6979a1a48
James Forster <14073802+jameshforster@users.noreply.github.com>
2023-10-23 00:58:16 +0100

    Added missing button component to fix final styling and removed auto minimise due to needing button to launch.

--- app/assets/javascripts/utils/ClickToChatButtons.js
+++ app/assets/javascripts/utils/ClickToChatButtons.js
@@ -35,7 +35,7 @@ export default class ClickToChatButtons {
         let innerHTML = ``
 
         if (hmrcSkin) {
-            innerHTML = `<div id="ciapiSkinMinimised"><div id="logo-white"><img src="/engagement-platform-skin/assets/media/logo-white.png"></div><h2 class="govuk-heading-s govuk-!-font-size-19">Ask HMRC a Question</h2></div>`
+            innerHTML = `<button id="ciapiSkinRestoreButton" type="button" draggable="false" role="button" tabindex="0"><div id="ciapiSkinMinimised"><div id="logo-white"><img src="/engagement-platform-skin/assets/media/logo-white.png"></div><h2 class="govuk-heading-s govuk-!-font-size-19">Ask HMRC a Question</h2></div></button>`
         } else {
             innerHTML = `<div class="${button.buttonClass} ${c2cObj.displayState}">${buttonText}</div>`;
         }
