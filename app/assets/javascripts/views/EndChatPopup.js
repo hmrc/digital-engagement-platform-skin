f8faaf9eb844622277905f5eec028df20e846d4e
James Forster <14073802+jameshforster@users.noreply.github.com>
2023-11-17 13:53:31 +0000

    DEP-5805 Updated libraries and added missing class to survey links.

--- app/assets/javascripts/views/EndChatPopup.js
+++ app/assets/javascripts/views/EndChatPopup.js
@@ -11,7 +11,7 @@ export const popupHtml = `
       </button>
 
       <div id="surveyPrintContainer">
-    <p><a href="#" id="printLink">Print or save your chat</a>.</p>
+    <p><a class="govuk-link" href="#" id="printLink">Print or save your chat</a>.</p>
   </div>
 
     </div>
