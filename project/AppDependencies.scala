b72ab5e152fc841db9021c1391cbbe4bdc4ee92f
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2023-11-24 11:07:15 +0000

    DEP-5820 proactive popup functionality fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "7.23.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "7.19.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "6.0.0-play-28",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.35.10"         % "test"
   )
