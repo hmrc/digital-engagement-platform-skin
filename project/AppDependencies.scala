b74c2a8ea948b8813a99c35b5e1366c10c487e21
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2023-11-24 11:12:11 +0000

    DEP-5820 refactor

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "7.19.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "7.23.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "6.0.0-play-28",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.35.10"         % "test"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.23.0"            % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.19.0"            % Test,
     "org.jsoup"               %  "jsoup"                      % "1.15.1"            % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
   )
