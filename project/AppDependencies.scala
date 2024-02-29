40619ad0c0d7547121c56d5453ad4ad280f0c75b
dantate92 <daniel.tate@digital.hmrc.gov.uk>
2024-02-29 12:04:32 +0000

    further changes for upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-29" % "8.4.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "8.4.0-play-29",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-29" % "8.4.0",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.35.10"         % "test"
   )
 
