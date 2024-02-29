ebf3aab4ccc8f78ffa909a825c7ca8f772e8b0d7
dantate92 <daniel.tate@digital.hmrc.gov.uk>
2024-02-29 15:41:11 +0000

    removing dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,12 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-29" % "8.4.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-29" % "8.4.0",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.35.10"         % "test"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-29" % "8.4.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-29"     % "8.4.0"            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.15.1"            % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-29"     % "8.4.0"             % Test,
+    "org.jsoup"               %  "jsoup"                      % "1.15.1"            % Test
   )
 
   val all: Seq[ModuleID] = compile ++ test
