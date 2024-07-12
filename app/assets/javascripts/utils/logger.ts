382e0fb23f2f2639d98eed786a223d506d410304
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2024-07-12 15:40:33 +0100

    Converted logger to TS.

--- app/assets/javascripts/utils/logger.ts
+++ app/assets/javascripts/utils/logger.ts
@@ -1,39 +1,34 @@
-function envChecker() {
-    let env;
-    let url = window.location.href;
+function envChecker(): string {
+    let env: string;
+    let url: string = window.location.href;
 
     if(url.includes('qa') || url.includes('test') || url.includes('staging') || url.includes('localhost')) {
         env = 'dev'
     } else {
         env = 'prod'
     }
-
     return env;
 }
 
-export const info = (info) =>  {
+export const info = (info: string): void =>  {
 
     if(envChecker() == 'dev'){
         console.log('INFO: ' + info)
-    } 
-    
+    }  
 }
 
-export const debug = (info, obj) =>  {
-
+export const debug = (info: string, obj: {}): void =>  {
     if(envChecker() == 'dev'){
         console.log('DEBUG: ' + info, obj)
     } 
 }
 
-export const error = (info, obj) =>  {
-
+export const error = (info: string, obj: {}): void =>  {
     if(envChecker() == 'dev'){
         if(obj){
             console.error('ERROR: ' + info, obj)
         } else {
             console.error('ERROR: ' + info)
         }
-        
     } 
 }
