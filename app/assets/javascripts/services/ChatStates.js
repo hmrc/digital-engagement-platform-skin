987958e1de440b801918977524c5420630498626
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2024-08-06 16:38:49 +0100

    DEP-6534 moved displayState check to anchored scenario only + readded default messages

--- app/assets/javascripts/services/ChatStates.js
+++ app/assets/javascripts/services/ChatStates.js
@@ -257,13 +257,13 @@ export class EngagedState {
                 transcript.addSystemMsg({msg: (msg["display.text"] || messages.adviserExitedChat)}, msg.messageTimestamp);
                 break;
             case MessageType.Chat_CommunicationQueue:
-                transcript.addSystemMsg({msg: msg.messageText}, msg.messageTimestamp);
+                transcript.addSystemMsg({msg: (msg.messageText || messages.agentBusy)}, msg.messageTimestamp);
                 break;
             case MessageType.Chat_NeedWait:
-                transcript.addSystemMsg({msg: msg.messageText}, msg.messageTimestamp);
+                transcript.addSystemMsg({msg: (msg.messageText || messages.queue1 + msg["queueDepth"] + messages.queue2)}, msg.messageTimestamp);
                 break;
             case MessageType.Chat_Denied:
-                transcript.addSystemMsg({msg: msg["thank_you_image_label"]}, msg.messageTimestamp);
+                transcript.addSystemMsg({msg: (msg["thank_you_image_label"] || messages.adviserUnavailable)}, msg.messageTimestamp);
                 break;
             case MessageType.ChatRoom_MemberLost:
                 this._chatRoomMemberLost(msg, transcript);
