98baf20a6602d0a737d3f9bb26ccb15fd923af7a
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2024-11-21 14:01:03 +0000

    Removed the box from saying you are speaking to a computer on C2C.

--- app/assets/javascripts/utils/ClickToChatButtons.ts
+++ app/assets/javascripts/utils/ClickToChatButtons.ts
@@ -44,9 +44,9 @@ export default class ClickToChatButtons {
         let innerHTML: string = ``
 
         if (isAnchored) {
-            innerHTML = `<div id="ciapiSkinMinimised"><button id="ciapiSkinRestoreButton" type="button" draggable="false" role="button" tabindex="0"><h2 class="govuk-heading-s govuk-!-font-size-19">Ask HMRC a Question</h2></button></div>`
+            innerHTML = `<div class="webchat-only" id="ciapiSkinMinimised"><button id="ciapiSkinRestoreButton" type="button" draggable="false" role="button" tabindex="0"><h2 class="govuk-heading-s govuk-!-font-size-19">Ask HMRC a Question</h2></button></div>`
         } else {
-            innerHTML = `<div class="${c2cObj.displayState}">${buttonText}</div>`;
+            innerHTML = `<div class="${c2cObj.displayState} webchat-only">${buttonText}</div>`;
         }
 
         const div: HTMLElement | undefined = button.replaceChild(innerHTML, isAnchored);
