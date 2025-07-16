a41962e3fd7ed1c6ecc3d7d8a55f400b28162ff0
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-07-16 15:45:10 +0100

    Set up the ajaxget and keepAlive functions which are running and hitting the test end point.

--- app/assets/javascripts/controllers/CommonChatController.ts
+++ app/assets/javascripts/controllers/CommonChatController.ts
@@ -460,6 +460,28 @@ export default class CommonChatController {
             this.onScreenReaderMessageSentNotification()
             this.container.clearCurrentInputText();
         }
+        //if (window.location.href.includes('personal-account')) {
+        this.keepAliveAndClose();
+        //}
+    }
+
+    keepAliveAndClose = () => {
+        // cleanup();
+        // setupDialogTimer();
+        this.ajaxGet("/ask-hmrc/test-only/keep-alive", () => { });
+        // broadcastSessionActivity();
+    };
+
+    ajaxGet(url: any, success: any) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState > 3 && xhr.status === 200) success(xhr.responseText);
+            console.log('CONDITION true')
+        };
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.send();
+        return xhr;
     }
 
     onCloseChat(): void {
