d6104f1845821317ddcba41036eadc6aef122c46
Andrew Learmouth <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-09-09 15:43:36 +0100

    DEP-7122: Updated bootstrap to 9 19 0 (#332)
    * Updated bootstrap to 9 19 0
    
    * DEP-7122 updating ErrorHandler
    
    * DEP-7122 pr bot comment
    
    ---------
    
    Co-authored-by: jacksgannon <19840977+jacksgannon@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % "8.6.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % "9.19.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "9.11.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "8.6.0"             % Test
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.19.0"             % Test
   )
 
   val all: Seq[ModuleID] = compile ++ test
