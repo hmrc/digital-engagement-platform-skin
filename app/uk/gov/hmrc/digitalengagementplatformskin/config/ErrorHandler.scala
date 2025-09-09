e734c7953e7331bf472be9ea1412b06fe8e418a0
jacksgannon <19840977+jacksgannon@users.noreply.github.com>
2025-09-09 09:53:26 +0100

    DEP-7122 updating ErrorHandler

--- app/uk/gov/hmrc/digitalengagementplatformskin/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/digitalengagementplatformskin/config/ErrorHandler.scala
@@ -17,18 +17,20 @@
 package uk.gov.hmrc.digitalengagementplatformskin.config
 
 import javax.inject.{Inject, Singleton}
-
 import play.api.i18n.MessagesApi
-import play.api.mvc.Request
+import play.api.mvc.{Request, RequestHeader}
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import uk.gov.hmrc.digitalengagementplatformskin.views.html.ErrorTemplate
 
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
 class ErrorHandler @Inject()(val messagesApi: MessagesApi,
-                             errorTemplate: ErrorTemplate)
+                             errorTemplate: ErrorTemplate,
+                             implicit val ec: ExecutionContext)
     extends FrontendErrorHandler {
 
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html =
-    errorTemplate(pageTitle, heading, message)
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: RequestHeader): Future[Html] =
+    Future.successful(errorTemplate(pageTitle, heading, message))
 }
