92573135e4c618e40c16be2ea295aa4809c8c9d1
Ahmet-Erdogan <118400556+Ahmet-Erdogan@users.noreply.github.com>
2025-09-29 14:19:51 +0100

    play-frontend-hmrc-play-30 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % "9.19.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.6.0"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.13.0"
   )
 
   val test = Seq(
